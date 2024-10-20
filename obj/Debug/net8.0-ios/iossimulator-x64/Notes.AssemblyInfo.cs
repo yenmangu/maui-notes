41fabde505a6b8510c346ff979864d3e07ee50cb
yenmangu <robert.shelford@googlemail.com>
2024-10-20 02:41:22 +0100

    Initial Commit - app started with dotnet cli tools

--- obj/Debug/net8.0-ios/iossimulator-x64/Notes.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/Notes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Notes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ce9247cac5deacfb40bf4f4ff2cc94892eb21498")]
 [assembly: System.Reflection.AssemblyProductAttribute("Notes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Notes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
